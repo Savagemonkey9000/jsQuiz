932455f467d5e8b5b832ec9197d80a6647ea95ef
savagemonkey9000 <headbangersalt@mail.com>
2023-12-13 22:28:22 +0100

    fourth commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = [
+    { question: "Fråga 1: Himlen är en social konstruktion", answer: "Falskt" },
+    { question: "Fråga 2: Jultomten betalar skatt?", answer: "Falskt" },
+    { question: "Fråga 3: socker är surt?", answer: "Falskt" },
+    { question: "Fråga 4: Det är kallt utomhus?", answer: "Sant" },
+    { question: "Fråga 5: Saker blir dyrare?", answer: "Sant" },
+    { question: "Fråga 6: Det gör ont att ramla på betong?", answer: "Sant" },
+    { question: "Fråga 7: En spade är en spade?", answer: "Sant" },
+    { question: "Fråga 8: internet är en serie av tuber?", answer: "Sant" },
+    { question: "Fråga 9:  Ramen är objektivt den bästa maträtten", answer: "Sant" },
+    { question: "Fråga 10: Är jorden rund?", answer: "Sant" }
+  ];
