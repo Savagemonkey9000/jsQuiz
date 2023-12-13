60044455e9656e79e59956077a3d4467b00dd5d4
savagemonkey9000 <headbangersalt@mail.com>
2023-12-13 22:33:23 +0100

    fifth commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const questions = [
+const questionsArr = [
     { question: "Fråga 1: Himlen är en social konstruktion", answer: "Falskt" },
     { question: "Fråga 2: Jultomten betalar skatt?", answer: "Falskt" },
     { question: "Fråga 3: socker är surt?", answer: "Falskt" },
@@ -10,3 +10,25 @@ const questions = [
     { question: "Fråga 9:  Ramen är objektivt den bästa maträtten", answer: "Sant" },
     { question: "Fråga 10: Är jorden rund?", answer: "Sant" }
   ];
+
+  function createQuestionElement(question, index) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+          <p>${question.question}</p>
+          <label><input type="radio" name="question${index}" value="Sant"> Sant</label>
+          <label><input type="radio" name="question${index}" value="Falskt"> Falskt</label>
+      `;
+    return div;
+  }
+  function calculateResult() {
+    let score = 0;
+    questions.forEach((question, index) => {
+      const selected = document.querySelector(
+        `input[name="question${index}"]:checked`
+      );
+      if (selected && selected.value === question.answer) {
+        score++;
+      }
+    });
+    return score;
+  }
